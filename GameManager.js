f7b8c25b5bba90303a75f086414efab263fa9e1c
Felipe Massao <massao.felipe@gmail.com>
2020-03-08 16:40:50 -0300

    implemented window.requestAnimationFrame logic

--- GameManager.js
+++ GameManager.js
@@ -5,22 +5,35 @@ class GameManager {
 		this.height = canvas.height;
 		this.width = canvas.width;
 		this.ctx = canvas.getContext('2d');
+		this.i = 0;
+		// this.updateFrame = this.updateFrame.bind(this);
 	}
 
 	start() {
-		let { ctx, height, width } = this;
+		window.requestAnimationFrame(() => this.updateFrame());
+		console.log('Game Started');
+	}
+
+	updateFrame() {
+		this.draw();
+		window.requestAnimationFrame(() => this.updateFrame());
+	}
+
+	draw() {
+		let { ctx, height, width, i } = this;
+
+		ctx.clearRect(0, 0, width, height);
 
 		ctx.beginPath();
 		ctx.strokeStyle = 'black';
 		let radius = height / 20;
-		ctx.arc(width / 2, height / 2, radius, 0, Math.PI * 2, true);
+		ctx.arc(width / 2, height / 2, radius + i, 0, Math.PI * 2, true);
 		ctx.stroke();
 		ctx.closePath();
+		this.i = i + 0.1;
 
 		this.player = new Player(height / 2, width / 2, 20);
 		this.player.draw(ctx);
-
-		console.log('Game Started');
 	}
 }
 
