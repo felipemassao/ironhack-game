a972be8dd5801ad54b167d72ed67b4a0db1f9e5a
Felipe Massao <massao.felipe@gmail.com>
2020-03-08 15:42:37 -0300

    drawing a circle in center

--- GameManager.js
+++ GameManager.js
@@ -2,16 +2,21 @@
 class GameManager {
 	constructor(canvas) {
 		this.canvas = canvas;
+		this.height = canvas.height;
+		this.width = canvas.width;
 		this.ctx = canvas.getContext('2d');
 	}
 
 	gameStart() {
-		let { ctx } = this;
-		this.player = new Player();
-		this.block = new Block();
+		let { ctx, height, width } = this;
+
+		ctx.beginPath();
+		ctx.strokeStyle = 'black';
+		let radius = height / 20;
+		ctx.arc(width / 2, height / 2, radius, 0, Math.PI * 2, true);
+		ctx.stroke();
+
 		console.log('Game Started');
-		ctx.fillStyle = 'red';
-		ctx.fillRect(200, 200, 200, 200);
 	}
 }
 
