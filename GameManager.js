aeb77ff81efcdfc4c71adee596df6f0d0496d680
Felipe Massao <massao.felipe@gmail.com>
2020-03-08 16:17:12 -0300

    drawing player in the center of canvas

--- GameManager.js
+++ GameManager.js
@@ -7,7 +7,7 @@ class GameManager {
 		this.ctx = canvas.getContext('2d');
 	}
 
-	gameStart() {
+	start() {
 		let { ctx, height, width } = this;
 
 		ctx.beginPath();
@@ -15,13 +15,41 @@ class GameManager {
 		let radius = height / 20;
 		ctx.arc(width / 2, height / 2, radius, 0, Math.PI * 2, true);
 		ctx.stroke();
+		ctx.closePath();
+
+		this.player = new Player(height / 2, width / 2, 20);
+		this.player.draw(ctx);
 
 		console.log('Game Started');
 	}
 }
 
 // Class who represents the Player and contains the game logic for inputs
-class Player {}
+class Player {
+	constructor(x, y, side) {
+		this.side = side;
+		this.setPosition(x, y);
+	}
+
+	setPosition(x, y) {
+		let { side } = this;
+		this.x = x - side / 2;
+		this.y = y - side / 2;
+	}
+
+	getPosition() {
+		return {
+			x: this.x,
+			y: this.y
+		};
+	}
+
+	draw(ctx) {
+		let { x, y, side } = this;
+		ctx.strokeStyle = 'blue';
+		ctx.strokeRect(x, y, side, side);
+	}
+}
 
 // Blocks the player must avoid
 class Block {}
--- game.js
+++ game.js
@@ -2,5 +2,5 @@ let canvas = document.querySelector('canvas');
 let gameManager = new GameManager(canvas);
 
 document.getElementById('start').addEventListener('click', (event) => {
-	gameManager.gameStart();
+	gameManager.start();
 });
